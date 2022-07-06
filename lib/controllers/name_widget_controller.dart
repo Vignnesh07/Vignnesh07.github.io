248dab39c2a8ea169a2d7e6af36b3da7c3b4ac7a
Vignnesh <ravin.vignnesh@gmail.com>
2022-07-07 00:18:16 +0800

    .

--- lib/controllers/name_widget_controller.dart
+++ lib/controllers/name_widget_controller.dart
@@ -9,7 +9,10 @@ class NameWidgetController extends GetxController {
 
   RxDouble nameScrollOffset = 0.0.obs;
   RxDouble projectScrollOffset = 0.0.obs;
-  late ScrollController scrollController;
+
+  late ScrollController nestedScrollController;
+  late ScrollController projectsScrollController;
+
   RxInt currentlyDisplayed = 0.obs;
   List<ProjectModel> projectList = [
     ProjectModel(no: 1, name: 'TBSB', role: 'Full Stack Developer', description: 'Provides production insight and real-time inventory tracking', stacks: ['Flutter', 'Firebase'], appType: 'Android App', picUrl: ['assets/images/TBSB/1.png', 'assets/images/TBSB/2.png', 'assets/images/TBSB/3.png', 'assets/images/TBSB/4.png']),
@@ -19,24 +22,95 @@ class NameWidgetController extends GetxController {
 
   @override
   void onInit() {
-    scrollController = ScrollController()
+    nestedScrollController = ScrollController()
     ..addListener(() {
-      nameScrollOffset.value = scrollController.offset;
-      print("Name Offset: ${nameScrollOffset.value}");
+      nameScrollOffset.value = nestedScrollController.offset;
+      debugPrint("Name Offset: ${nameScrollOffset.value}");
     });
-    
+    projectsScrollController = ScrollController();
     super.onInit();
+  }
+
+  @override
+  void onReady() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      
+      // nameScrollOffset.listen((offset) {
+      //   if (offset >= 1188) {
+      //     debugPrint('working');
+      //     nestedScrollController.jumpTo(nestedScrollController.position.maxScrollExtent);
+      //     // nestedScrollController.animateTo(nestedScrollController.position.maxScrollExtent, duration: const Duration(seconds: 1), curve: Curves.easeIn);
+      //   }
+      // });
+
+      projectScrollOffset.listen((offset) {
+        if (offset >= 130) {
+          currentlyDisplayed.value = 0;
+        }
+        else if (offset >= 940){
+          currentlyDisplayed.value = 1;
+        }
+        else if (offset >= 1710){
+          currentlyDisplayed.value = 1;
+        }
+        else {
+          currentlyDisplayed.value = 0;
+        }
+      });
+
+      debugPrint(currentlyDisplayed.value.toString());
+
+    });
+  }
+
+  alterCurrentDisplayedProject(){
+    if (projectScrollOffset.value >= 130.00){
+      currentlyDisplayed.value = 0;
+    }
+
+    else if (projectScrollOffset.value >= 2437.00){
+      currentlyDisplayed.value = 1;
+    }
+
+    else if (projectScrollOffset.value >= 1720.00){
+      currentlyDisplayed.value = 2;
+    }
+
+    else {
+      currentlyDisplayed.value = 0;
+    }
+    debugPrint("Currently Displayed: ${currentlyDisplayed.value}");
   }
 
   /// Returns true to cancel the notification bubbling.
   bool updateNameOffset(ScrollNotification scrollNotification) {
-    // if (scrollNotification.metrics.pixels >= 1577) {
-      projectScrollOffset.value = scrollNotification.metrics.pixels;
-      print("Project: ${projectScrollOffset.value}");
-    // }
-    
-    // print("Offset: ${nameWidgetOffset.value}");
-    // print("Metrics: ${scrollNotification.metrics.pixels}");
+    // projectScrollOffset.value = scrollNotification.metrics.pixels;
+    // alterCurrentDisplayedProject(projectScrollOffset.value);
+    // print("Project Offset: ${projectScrollOffset.value}");
+    // print("Currently Displayed: ${currentlyDisplayed.value}");
+    // return true;
+
+    if (scrollNotification is ScrollUpdateNotification) {
+      if (scrollNotification.metrics.pixels == scrollNotification.metrics.maxScrollExtent) {
+        debugPrint('Reached the bottom');
+        nestedScrollController.animateTo(
+          nestedScrollController.position.maxScrollExtent,
+          duration: const Duration(seconds: 1),
+          curve: Curves.easeIn,
+        );
+      } 
+      else if (scrollNotification.metrics.pixels == scrollNotification.metrics.minScrollExtent) {
+        debugPrint('Reached the top');
+        nestedScrollController.animateTo(
+          nestedScrollController.position.minScrollExtent,
+          duration: const Duration(seconds: 1),
+          curve: Curves.easeIn,
+        );
+      }
+    }
+    projectScrollOffset.value = scrollNotification.metrics.pixels;
+    alterCurrentDisplayedProject();
+    debugPrint("Project offset: ${projectScrollOffset.value.toString()}");
     return true;
   }
 }
--- lib/widgets/project1.dart
+++ lib/widgets/project1.dart
@@ -14,132 +14,207 @@ class Project1Widget extends StatelessWidget {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
 
-    return Flexible(
-      fit: FlexFit.loose,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            flex: 4,
-            child: SizedBox(
-              height: height,
-              child: SingleChildScrollView(
-                physics: const NeverScrollableScrollPhysics(),
-                primary: false,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                      alignment: Alignment.topLeft,
-                      padding: const EdgeInsets.only(left: 35.0, top: 40.0),
-                      child: Text(
-                        nameWidgetController.projectList[nameWidgetController.currentlyDisplayed.value].no.toString().padLeft(2, '0'),
-                        style: TextStyle(
-                          height: 0.5,
-                          fontSize: 25,
-                          color: Colors.black.withOpacity(1.0),
-                          fontFamily: 'Metropolis-SemiBold', 
-                          fontWeight: FontWeight.w900,
-                        ),
-                        textHeightBehavior: const TextHeightBehavior(applyHeightToFirstAscent: true),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 4,
-            child: SingleChildScrollView(
-              child: SizedBox(
-                height: height * (nameWidgetController.projectList.length + 2.4),
-                child: ListView.separated(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  padding: const EdgeInsets.all(0),
-                  separatorBuilder: (_, __) => const SizedBox(height: 0.0), 
-                  itemCount: nameWidgetController.projectList.length,
-                  itemBuilder: (context, index) {
-                    return SizedBox(
-                      height: index == 1 ? height / 2 * (index + 1) : index == 2 ? height / 7 * (index + 1) : height * (index + 1),
-                      child: Stack(
-                        clipBehavior: Clip.none,
-                        alignment: Alignment.topCenter,
-                        children: [
-                          Obx(() => AnimatedPositionedDirectional(
-                            curve: Curves.linearToEaseOut,
-                            duration: const Duration(milliseconds: 500),
-                            start: -30.0,
-                            bottom: index == 0 ? nameWidgetController.projectScrollOffset.value - height * (index + 2.22) : index == 1 ? nameWidgetController.projectScrollOffset.value - height * (index + 2.00) : nameWidgetController.projectScrollOffset.value - height * (index + 2.45),
-                            child: Container(
-                              height: height / 1.55,
-                              width: 500,
-                              clipBehavior: Clip.antiAlias,
-                              decoration: BoxDecoration(
-                                image: DecorationImage(image: AssetImage(nameWidgetController.projectList[index].picUrl[2]), fit: BoxFit.scaleDown)
-                              ),
-                              child: BackdropFilter(
-                                filter: ImageFilter.blur(sigmaX: 0.1, sigmaY: 0.1),
-                                child: Container(
-                                  decoration: BoxDecoration(color: Colors.white.withOpacity(0.0)),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Flexible(
+          fit: FlexFit.loose,
+          child: SizedBox(
+            height: height,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 4,
+                  child: SizedBox(
+                    height: height,
+                    child: SingleChildScrollView(
+                      primary: false,
+                      physics: const NeverScrollableScrollPhysics(),
+                      child: ConstrainedBox(
+                        constraints: BoxConstraints(minHeight: height, maxHeight: height),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Obx(() => Container(
+                              alignment: Alignment.topLeft,
+                              padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 50),
+                              child: Text(
+                                nameWidgetController.projectList[nameWidgetController.currentlyDisplayed.value].no.toString().padLeft(2, '0'),
+                                textHeightBehavior: const TextHeightBehavior(applyHeightToFirstAscent: true),
+                                style: TextStyle(
+                                  height: 0.5,
+                                  fontSize: 25,
+                                  color: Colors.black.withOpacity(1.0),
+                                  fontFamily: 'Avenir-Heavy',
+                                  fontWeight: FontWeight.w900
                                 ),
                               ),
-                            ),
-                          ),),
-                          Obx(() => AnimatedPositionedDirectional(
-                            curve: Curves.linearToEaseOut,
-                            duration: const Duration(milliseconds: 300),
-                            start: 30.0,
-                            bottom: index == 0 ? nameWidgetController.projectScrollOffset.value - height * (index + 1.40) : index == 1 ? nameWidgetController.projectScrollOffset.value - height * (index + 1.40) : nameWidgetController.projectScrollOffset.value - height * (index + 1.85),
-                            child: Container(
-                              height: height / 1.13,
-                              width: 500,
-                              clipBehavior: Clip.antiAlias,
-                              decoration: BoxDecoration(
-                                image: DecorationImage(image: AssetImage(nameWidgetController.projectList[index].picUrl[0]), fit: BoxFit.scaleDown, alignment: Alignment.topLeft, filterQuality: FilterQuality.high,),
+                            ),),
+                            const SizedBox(height: 60.0),
+                            Obx(() => Container(
+                              alignment: Alignment.centerLeft,
+                              padding: const EdgeInsets.symmetric(horizontal: 65.0),
+                              child: Text(
+                                nameWidgetController.projectList[nameWidgetController.currentlyDisplayed.value].name,
+                                textAlign: TextAlign.left,
+                                style: const TextStyle(
+                                  fontSize: 70,
+                                  color: Color.fromARGB(255, 54, 54, 54),
+                                  fontFamily: 'Avenir-Heavy', 
+                                ),
                               ),
-                            ),
-                          ),),
-                          Obx(() => AnimatedPositionedDirectional(
-                            curve: Curves.linearToEaseOut,
-                            duration: const Duration(milliseconds: 500),
-                            end: -50.0,
-                            bottom: index == 0 ? nameWidgetController.projectScrollOffset.value - height * (index + 2.20) : index == 1 ? nameWidgetController.projectScrollOffset.value - height * (index + 2.00) : nameWidgetController.projectScrollOffset.value - height * (index + 2.48),
-                            child: Container(
-                              height: height / 1.75,
-                              width: 500,
-                              clipBehavior: Clip.antiAlias,
-                              decoration: BoxDecoration(
-                                image: DecorationImage(image: AssetImage(nameWidgetController.projectList[index].picUrl[3]), fit: BoxFit.scaleDown)
+                            ),),
+                            const SizedBox(height: 30.0),
+                            Obx(() => Container(
+                              alignment: Alignment.centerLeft,
+                              padding: const EdgeInsets.symmetric(horizontal: 65.0),
+                              child: Text(
+                                nameWidgetController.projectList[nameWidgetController.currentlyDisplayed.value].role,
+                                textAlign: TextAlign.left,
+                                style: const TextStyle(
+                                  fontSize: 25,
+                                  color: Color.fromARGB(255, 54, 54, 54),
+                                  fontFamily: 'Avenir-Medium', 
+                                ),
                               ),
-                            ),
-                          ),),
-                          Obx(() => AnimatedPositionedDirectional(
-                            curve: Curves.linearToEaseOut,
-                            duration: const Duration(milliseconds: 400),
-                            end: 30.0,
-                            bottom: index == 0 ? nameWidgetController.projectScrollOffset.value - height * (index + 1.65) : index == 1 ? nameWidgetController.projectScrollOffset.value - height * (index + 1.45) : nameWidgetController.projectScrollOffset.value - height * (index + 1.95),
-                            child: Container(
-                              alignment: Alignment.topRight,
-                              height: height / 1.27,
-                              width: 500,
-                              clipBehavior: Clip.antiAlias,
-                              decoration: BoxDecoration(
-                                image: DecorationImage(image: AssetImage(nameWidgetController.projectList[index].picUrl[1]), fit: BoxFit.scaleDown, alignment: Alignment.topRight,)
+                            ),),
+                            const SizedBox(height: 15.0),
+                            Obx(() => Container(
+                              alignment: Alignment.centerLeft,
+                              padding: const EdgeInsets.symmetric(horizontal: 65.0),
+                              child: Text(
+                                nameWidgetController.projectList[nameWidgetController.currentlyDisplayed.value].description,
+                                textAlign: TextAlign.left,
+                                softWrap: true,
+                                style: const TextStyle(
+                                  fontSize: 25,
+                                  color: Color.fromARGB(255, 54, 54, 54),
+                                  fontFamily: 'Avenir-Book', 
+                                ),
                               ),
-                            ),
-                          ),),
-                        ],
+                            ),),
+                            const Spacer(),
+                            Obx(() => AnimatedContainer(
+                              curve: const Cubic(0.19, 1, 0.22, 1),
+                              duration: const Duration(seconds: 1),
+                              alignment: Alignment.bottomLeft,
+                              padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 30.0),
+                              child: Text(
+                                nameWidgetController.projectList[nameWidgetController.currentlyDisplayed.value].appType.toUpperCase(),
+                                textAlign: TextAlign.left,
+                                style: const TextStyle(
+                                  fontSize: 25,
+                                  color: Color.fromARGB(255, 54, 54, 54),
+                                  fontFamily: 'Avenir-Heavy', 
+                                ),
+                              ),
+                            ),),
+                          ],
+                        ),
                       ),
-                    );
-                  }, 
+                    ),
+                  ),
                 ),
-              ),
-            )
-          )
-        ],
-      ),
+                Expanded(
+                  flex: 4,
+                  child: NotificationListener<ScrollNotification>(
+                    onNotification: nameWidgetController.updateNameOffset,
+                    child: SingleChildScrollView(
+                      child: SizedBox(
+                        height: height * (nameWidgetController.projectList.length + 2.4),
+                        child: ListView.separated(
+                          shrinkWrap: true,
+                          controller: nameWidgetController.projectsScrollController,
+                          physics: const NeverScrollableScrollPhysics(),
+                          padding: const EdgeInsets.all(0),
+                          separatorBuilder: (_, __) => const SizedBox(height: 0.0), 
+                          itemCount: nameWidgetController.projectList.length,
+                          itemBuilder: (context, index) {
+                            return SizedBox(
+                              height: index == 1 ? height / 2 * (index + 1) : index == 2 ? height / 7 * (index + 1) : height * (index + 1),
+                              child: Stack(
+                                clipBehavior: Clip.none,
+                                alignment: Alignment.topCenter,
+                                children: [
+                                  Obx(() => AnimatedPositionedDirectional(
+                                    curve: Curves.linearToEaseOut,
+                                    duration: const Duration(milliseconds: 500),
+                                    start: -30.0,
+                                    bottom: index == 0 ? nameWidgetController.projectScrollOffset.value - height * (index + 1.42) : index == 1 ? nameWidgetController.projectScrollOffset.value - height * (index + 1.45) : nameWidgetController.projectScrollOffset.value - height * (index + 2.10),
+                                    child: Container(
+                                      height: height / 1.55,
+                                      width: 500,
+                                      clipBehavior: Clip.antiAlias,
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(image: AssetImage(nameWidgetController.projectList[index].picUrl[2]), fit: BoxFit.scaleDown)
+                                      ),
+                                      child: BackdropFilter(
+                                        filter: ImageFilter.blur(sigmaX: 0.1, sigmaY: 0.1),
+                                        child: Container(
+                                          decoration: BoxDecoration(color: Colors.white.withOpacity(0.0)),
+                                        ),
+                                      ),
+                                    ),
+                                  ),),
+                                  Obx(() => AnimatedPositionedDirectional(
+                                    curve: Curves.linearToEaseOut,
+                                    duration: const Duration(milliseconds: 300),
+                                    start: 30.0,
+                                    bottom: index == 0 ? nameWidgetController.projectScrollOffset.value - height * (index + 0.80) : index == 1 ? nameWidgetController.projectScrollOffset.value - height * (index + 0.85) : nameWidgetController.projectScrollOffset.value - height * (index + 1.50),
+                                    child: Container(
+                                      height: height / 1.13,
+                                      width: 500,
+                                      clipBehavior: Clip.antiAlias,
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(image: AssetImage(nameWidgetController.projectList[index].picUrl[0]), fit: BoxFit.scaleDown, alignment: Alignment.topLeft, filterQuality: FilterQuality.high,),
+                                      ),
+                                    ),
+                                  ),),
+                                  Obx(() => AnimatedPositionedDirectional(
+                                    curve: Curves.linearToEaseOut,
+                                    duration: const Duration(milliseconds: 500),
+                                    end: -50.0,
+                                    bottom: index == 0 ? nameWidgetController.projectScrollOffset.value - height * (index + 1.45) : index == 1 ? nameWidgetController.projectScrollOffset.value - height * (index + 1.48) : nameWidgetController.projectScrollOffset.value - height * (index + 2.11),
+                                    child: Container(
+                                      height: height / 1.75,
+                                      width: 500,
+                                      clipBehavior: Clip.antiAlias,
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(image: AssetImage(nameWidgetController.projectList[index].picUrl[3]), fit: BoxFit.scaleDown)
+                                      ),
+                                    ),
+                                  ),),
+                                  Obx(() => AnimatedPositionedDirectional(
+                                    curve: Curves.linearToEaseOut,
+                                    duration: const Duration(milliseconds: 400),
+                                    end: 30.0,
+                                    bottom: index == 0 ? nameWidgetController.projectScrollOffset.value - height * (index + 0.90) : index == 1 ? nameWidgetController.projectScrollOffset.value - height * (index + 0.95) : nameWidgetController.projectScrollOffset.value - height * (index + 1.57),
+                                    child: Container(
+                                      alignment: Alignment.topRight,
+                                      height: height / 1.27,
+                                      width: 500,
+                                      clipBehavior: Clip.antiAlias,
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(image: AssetImage(nameWidgetController.projectList[index].picUrl[1]), fit: BoxFit.scaleDown, alignment: Alignment.topRight,)
+                                      ),
+                                    ),
+                                  ),),
+                                ],
+                              ),
+                            );
+                          }, 
+                        ),
+                      ),
+                    ),
+                  )
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
